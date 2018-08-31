61c201d16fcb399abc36b2f8503c751dd3c7d27e
chexing <chexing@gmail.com>
2018-08-31 15:21:51 +0800

    add some script

---
+++ bin/pushFrameworkJar.sh
@@ -0,0 +1,15 @@
+#########################################################################
+# File Name: pushFrameworkJar.sh
+# Author: octopus
+# mail: octopus_work.163.com
+# Created Time: 2016年07月12日 星期二 16时00分31秒
+#########################################################################
+#!/bin/sh
+CCA=$1
+adb root
+adb remount
+sleep 5
+adb wait-for-device
+adb push out/target/product/$CCA/system/framework/arm/ /system/framework/
+adb push out/target/product/$CCA/system/framework/arm64/ /system/framework/
+adb push out/target/product/$CCA/system/framework/framework.jar /system/framework/
